40849730d4860e27f709c1e9fa1a94063944cdd9
PuKoren <koren@pukogames.com>
2015-03-12 19:58:04 +0100

    Parse input file and store it into mock

--- index.js
+++ index.js
@@ -54,19 +54,49 @@ function main(inputFile, outputFile) {
 	lineReader.eachLine(inputFile, function(line) {
 		if (i === 0) { //if i = 0, then this is the first line of the input file (datacenter configuration)
 			var config = line.split(' ');
+			//if first row does not contains 5 parameters, exits
+			if (config.length < 5) {
+				throw new Error('Bad input file. Line:', i);
+			}
+
 			datacenter = {
 				rows: config[0],
 				slots: config[1],
- 				pools: config[3],
- 				unavailables: [],
- 				servers: []
+				unavailableCount: parseInt(config[2]),
+				serversCount: parseInt(config[4]),
+				pools: config[3],
+				unavailables: [],
+				servers: []
 			};
-			console.log('Config:', datacenter);
-		}
+			//console.log('Datacenter config:', datacenter);
+		} else { //if not first line, we parse unavailable slots first then available slots
+			var position = line.split(' ');
+			if (position.length < 2) {
+				throw new Error('Bad input file. Line:', i);
+			}
+			if (i <= datacenter.unavailableCount) {
+				datacenter.unavailables.push({
+					row: line[0],
+					slot: line[1]
+				});
+			} else { //else we put server in list
+				datacenter.servers.push({
+					row: line[0],
+					slot: line[1]
+				});
+			}
+		} //end if first line
 
-		++i;
+		++i; //increment current line index
 	}).then(function() {
-		console.log('Finished parsing, launching the awesome...');
+		console.log('Server count:', datacenter.servers.length + '/' + datacenter.serversCount);
+		console.log('Unavailable slots count:', datacenter.unavailables.length + '/' + datacenter.unavailableCount);
+		if (datacenter.servers.length === datacenter.serversCount && datacenter.unvailables.length === datacenter.unavailableCount) {
+			console.log('Finished parsing, launching the awesome...');
+			
+		} else {
+			throw new Error('Something gone wrong with config (well formed but data not consistant).');
+		}
 	});
 }
 
