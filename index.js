69223a920d21eea0c66386da283a431f546af601
PuKoren <koren@pukogames.com>
2015-03-12 19:45:51 +0100

    Read datacenter configuration

--- index.js
+++ index.js
@@ -9,13 +9,62 @@ if (process.argv[2] === undefined) {
 
 var lineReader = require('line-reader');
 
+/**
+var mock = {
+ rows: 2,
+ slots: 5,
+ pools: 2,
+ unvailables: [
+   {
+     row: 0,
+     slot: 0
+   }
+ ],
+ servers: [
+   {
+     slots: 3,
+     capacity: 10
+   },
+   {
+     slots: 3,
+     capacity: 10
+   },
+   {
+     slots: 2,
+     capacity: 5
+   },
+   {
+     slots: 1,
+     capacity: 5
+   },
+   {
+     slots: 1,
+     capacity: 1
+   }
+ ]
+};
+*/
+
 function main(inputFile, outputFile) {
-	var Datacenter = {};
-	var Servers = [];
+	var datacenter = {};
+	var i = 0;
 
 	console.log('Processing input file', inputFile);
+
 	lineReader.eachLine(inputFile, function(line) {
-		console.log(line);
+		if (i === 0) { //if i = 0, then this is the first line of the input file (datacenter configuration)
+			var config = line.split(' ');
+			datacenter = {
+				rows: config[0],
+				slots: config[1],
+ 				pools: config[3],
+ 				unavailables: [],
+ 				servers: []
+			};
+			console.log('Config:', datacenter);
+		}
+
+		++i;
 	}).then(function() {
 		console.log('Finished parsing, launching the awesome...');
 	});
